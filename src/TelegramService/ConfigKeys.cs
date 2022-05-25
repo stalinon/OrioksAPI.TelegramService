6fea1176e2923a865513161b953ab6ec4d94fed4
Egor Fedorov <egor.fedorov@codestetic.com>
2022-05-25 17:13:49 +0300

    Реализовал добавление переменных окружения из файла local.env

---
+++ src/TelegramService/ConfigKeys.cs
@@ -0,0 +1,14 @@
+﻿namespace TelegramService;
+
+/// <summary>
+///     Ключи конфигурации
+/// </summary>
+internal sealed class ConfigKeys
+{
+    /// <summary>
+    ///     Путь к .env файлу
+    /// </summary>
+    public static readonly string EnvPath = "../../../../../local.env";
+
+    public static readonly string API_TOKEN = Environment.GetEnvironmentVariable(nameof(API_TOKEN))!;
+}
